24acfe3b7c33967d8b8eb4c417fc83215282ba2b
danielshinji <danielshinji.chandra@gmail.com>
2015-12-23 11:42:14 +0700

    new

---
+++ Assets/Scripts/BabyController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class BabyController : MonoBehaviour {
+
+    
+    private float babyPositionX;
+    private float babyPositionY;
+    private float currentPower;
+    private bool isShooting;
+
+    private GameObject babyClone;
+    private GameObject BabyClone
+    {
+        get
+        {
+            return babyClone;
+        }
+    }
+
+    public GameObject baby;
+
+    [HideInInspector] public Rigidbody2D rb;
+
+
+    public float powerChargeSpeed;
+    public Slider powerBar;
+
+
+    void Init()
+    {
+
+        powerBar = GetComponent<Slider>();
+        baby = GetComponent<GameObject>();
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+    
+    void Trajectory()
+    {
+
+    }
+
+    void PrepareShoot()
+    {
+        if (Input.GetKey("mouse 0") == true && currentPower <= powerBar.maxValue)
+        {
+            currentPower += Time.deltaTime * powerChargeSpeed;
+            powerBar.value = currentPower;
+            Debug.Log("current power is now: " + currentPower);
+            isShooting = false;
+        }
+
+        if (Input.GetKey("mouse 0") == false)
+        {
+            currentPower = 0;
+            powerBar.value = currentPower;
+            isShooting = true;
+        }
+    }
+
+    void Shoot()
+    {
+        if (isShooting == true)
+        {
+            babyClone = Instantiate(baby, transform.position, Quaternion.identity)as GameObject;
+            
+        }
+    }
+
+    void Update()
+    {
+        PrepareShoot();
+
+        Trajectory();
+    }
+
+}
---
+++ Assets/Scripts/SisterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SisterController : MonoBehaviour {
+
+    [HideInInspector]
+    public Rigidbody2D rb;
+
+    public float speed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(h, 0.0f, 0.0f);
+        rb.velocity = movement * speed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+}
